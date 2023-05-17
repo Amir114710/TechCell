ce372106702f35b8d53251298f5b0dd66cad21be
unknown <97888az@gmail.com>
2023-05-17 12:51:32 +0430

    first commit

---
+++ assets/js/search.js
@@ -0,0 +1,10 @@
+function search2(){
+    document.getElementById('search-container').style.display = 'block';
+    document.getElementById('search').style.display = 'none';
+    document.getElementById('close').style.display = 'block';
+}
+function close_search(){
+    document.getElementById('search-container').style.display = 'none';
+    document.getElementById('search').style.display = 'block';
+    document.getElementById('close').style.display = 'none';
+}
